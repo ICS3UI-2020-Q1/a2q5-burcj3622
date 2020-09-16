402020f936ca9db08f1b1429f717ee356de79a79
burcj3622 <burcj3622@wrdsb.ca>
2020-09-16 12:23:30 +0000

    fizzbuzz

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *fizzbuzz
+ * @author jason
  */
 public class Main {
 
@@ -9,7 +10,38 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // scanner for user input 
+    Scanner input = new Scanner(System.in);
+
+    //asks user for input
+    System.out.println("Please enter a number to play FizzBuzz");
     
+    //user input
+    int number = input.nextInt();
+
+    int fizz = (number%3);
+    int buzz = (number%5);
+
+    if (fizz == 0 && buzz == 0){
+    System.out.println("you should say fizzbuzz");
+    }
+    
+    else if (fizz == 0){
+      System.out.println("you should say fizz");
+    }
+    else if (buzz == 0){
+      System.out.println("you should say buzz");
+    }
+  else if (buzz != 0 && fizz != 0){
+    System.out.println("you should say "+ number);
   }
+    
+    
+  }   
+
 }
+    
+
+    
+  
+
